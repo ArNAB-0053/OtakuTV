155f1c7747a265b4bd63a0fa395d678e7475b582
Arnab Bhattacharyya <arnabbhattacharyya6294@gmail.com>
2024-09-12 18:59:31 +0530

    modify favorite

--- components/MinimalDetails/Favourite/AddFav.js
+++ components/MinimalDetails/Favourite/AddFav.js
@@ -11,33 +11,10 @@ import { useRouter } from "next/navigation";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const Fav = ({ animeID, imageUrl, animeName }) => {
-  const router = useRouter()
+  const router = useRouter();
   const { isSignedIn, user } = useUser();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [favoriteAdded, setFavoriteAdded] = useState(false);
-  const success = toast.success("Favorite added successfully.", {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-    transition: Bounce,
-  });
-
-  const errorToast = toast.error('🦄 Wow so easy!', {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-    transition: Bounce,
-    });
 
   // Use SWR to fetch favorite status
   const { data: favoriteData, mutate: mutateFavorites } = useSWR(
@@ -54,7 +31,7 @@ const Fav = ({ animeID, imageUrl, animeName }) => {
 
   const handleAddToFavorites = async () => {
     if (!isSignedIn) {
-      router.push('/sign-in')
+      router.push('/sign-in');
       return;
     }
     if (isSubmitting) return;
@@ -82,15 +59,45 @@ const Fav = ({ animeID, imageUrl, animeName }) => {
 
       if (response.ok) {
         setFavoriteAdded(true);
-        success
+        toast.success("Favorite added successfully.", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          transition: Bounce,
+        });
         mutateFavorites(); // Update SWR cache to reflect the new state
       } else {
-        errorToast
+        toast.error('Error adding favorite: ' + result.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          transition: Bounce,
+        });
         console.error("Error adding favorite:", result.message);
       }
     } catch (error) {
       console.error("Error:", error);
-      errorToast
+      toast.error('Error: ' + error.message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+      });
     } finally {
       setIsSubmitting(false);
     }
@@ -98,28 +105,28 @@ const Fav = ({ animeID, imageUrl, animeName }) => {
 
   return (
     <>
-    <button
-      onClick={handleAddToFavorites}
-      className={`opacity-100 rounded-t-none rounded-b-md absolute -bottom-10 w-full flex items-center justify-center gap-x-3 font-semibold uppercase tracking-tighter py-3 text-white text-sm xl:text-md ${
-        favoriteAdded
-          ? "bg-green-800 hover:bg-green-900"
-          : "bg-[#ff0000] hover:bg-red-600"
-      }`}
-      disabled={isSubmitting || favoriteAdded}
-    >
-      {!favoriteAdded ? (
-        <>
-          <FaHeartCirclePlus size={23} />
-          <p>{isSubmitting ? "Adding..." : "Add to Favorites"}</p>
-        </>
-      ) : (
-        <>
-          <FaHeartCircleCheck size={23} />
-          <p>Added to Favorites</p>
-        </>
-      )}
-    </button>
-    <ToastContainer />
+      <button
+        onClick={handleAddToFavorites}
+        className={`opacity-100 rounded-t-none rounded-b-md absolute -bottom-10 w-full flex items-center justify-center gap-x-3 font-semibold uppercase tracking-tighter py-3 text-white text-sm xl:text-md ${
+          favoriteAdded
+            ? "bg-green-800 hover:bg-green-900"
+            : "bg-[#ff0000] hover:bg-red-600"
+        }`}
+        disabled={isSubmitting || favoriteAdded}
+      >
+        {!favoriteAdded ? (
+          <>
+            <FaHeartCirclePlus size={23} />
+            <p>{isSubmitting ? "Adding..." : "Add to Favorites"}</p>
+          </>
+        ) : (
+          <>
+            <FaHeartCircleCheck size={23} />
+            <p>Added to Favorites</p>
+          </>
+        )}
+      </button>
+      <ToastContainer />
     </>
   );
 };
